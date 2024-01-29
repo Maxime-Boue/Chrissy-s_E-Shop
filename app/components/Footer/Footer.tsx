8b1095d134b2b381a51c87bc5a51c45cae0e88bf
Maxime-Boue <maxime.boue@oclock.school>
2024-01-29 16:19:56 +0100

    add FooterList component

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+import { AiFillInstagram } from "react-icons/ai";
+import { MdFacebook } from "react-icons/md";
+import Container from "../Container";
+import FooterList from "./FooterList";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-green-900 text-white text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-8 pb-4">
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Catégories</h3>
+            <Link href="#">Illustration</Link>
+            <Link href="#">Boucles d&apos;oreilles</Link>
+            <Link href="#">Linogravure</Link>
+            <Link href="#">Textile</Link>
+          </FooterList>
+          <FooterList>
+            <h3 className="text-base font-bold mb-2">Suivez-moi</h3>
+            <div className="flex gap-2">
+              <Link href="#">
+                <MdFacebook size={30} />
+              </Link>
+              <Link href="#">
+                <AiFillInstagram size={30} />
+              </Link>
+            </div>
+          </FooterList>
+        </div>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,5 +1,31 @@
+import { Major_Mono_Display } from "next/font/google";
+import Link from "next/link";
+import Container from "../Container";
+
+const majorMonoDisplay = Major_Mono_Display({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="sticky top-0 w-full bg-pink-200 z-30 shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex items-center justify-between gap-3 md-gap-0">
+            <Link href="/" className={majorMonoDisplay.className}>
+              Chrissy Ottenhof E-Shop
+            </Link>
+            <div className="hidden md:block">SearchBar</div>
+            <div className="flex items-center gap-8 md:gap-12">
+              <div>CartCount</div>
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
